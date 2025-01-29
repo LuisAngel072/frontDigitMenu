5db069ff25463540700587e9bb360811e78f46de
Victor <89050826+VictorRTXD@users.noreply.github.com>
2025-01-29 14:22:04 +0000

    feat(crud-empleados): creando inputs con sus estilos, falta tabla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AdministradorComponent } from './pages/administrador/administrador.component';
+import { CrudEmpleadosComponent } from './pages/administrador/crud-empleados/crud-empleados.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: LoginComponent
-    },{
-        path: "Administrador",
+    },
+    {
+        path: "administrador",
         component: AdministradorComponent
+    },
+    {
+        path: "empleados",
+        component: CrudEmpleadosComponent
     }
 ];
--- src/app/pages/administrador/crud-empleados/crud-empleados.component.ts
+++ src/app/pages/administrador/crud-empleados/crud-empleados.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../comun-componentes/header/header.component';
 
 @Component({
   selector: 'app-crud-empleados',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './crud-empleados.component.html',
   styleUrl: './crud-empleados.component.css'
 })
