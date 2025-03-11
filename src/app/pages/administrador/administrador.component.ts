97c740f3c55321536339dec5fadc34c60910357e
luisangel072 <luis.delatorre5745@alumnos.udg.mx>
2025-03-10 19:56:17 -0600

    fix(crud extras opciones) -> cruds extras y opciones no funcionales, requieren revision

--- src/app/pages/administrador/administrador.component.ts
+++ src/app/pages/administrador/administrador.component.ts
@@ -7,11 +7,13 @@ import { CategoriasComponent } from '../categorias/categorias.component';
 import { CrudIngredientesComponent } from './crud-ingredientes/crud-ingredientes.component';
 import { MesasComponent } from './mesas/mesas.component';
 import { VentasComponent } from './ventas/ventas.component';
-import { Ingredientes, Roles, Usuarios_has_roles } from '../../types';
+import { Extras, Ingredientes, Opciones, Roles, Usuarios_has_roles } from '../../types';
 import { UsuariosService } from '../../services/usuarios.service';
 import { RolesService } from '../../services/roles.service';
 import { CrudAgregarProductosComponent } from './crud-productos/crud-agregar-productos/crud-agregar-productos.component';
 import { IngredientesService } from '../../services/ingredientes.service';
+import { ExtrasService } from '../../services/extras.service';
+import { OpcionesService } from '../../services/opciones.service';
 @Component({
   selector: 'app-administrador',
   standalone: true,
@@ -39,18 +41,25 @@ export class AdministradorComponent {
   public usuarios: Usuarios_has_roles[] = [];
   public roles: Roles[] = [];
   public ingredientes: Ingredientes[] = [];
+  public extras: Extras[] = [];
+  public opciones: Opciones[] = [];
 
   constructor(
     private readonly usuariosService: UsuariosService,
     private readonly rolesService: RolesService,
-    private readonly ingredientesService: IngredientesService
+    private readonly ingredientesService: IngredientesService,
+    private readonly extrasService: ExtrasService,
+    private readonly opcionesService: OpcionesService,
   ) {}
 
   async ngOnInit() {
     this.roles = await this.rolesService.obtenerRoles();
     this.usuarios = await this.usuariosService.obtenerUsuariosYRoles();
     this.ingredientes = await this.ingredientesService.getIngredientes();
+    this.extras = await this.extrasService.getExtras();
+    this.opciones = await this.opcionesService.getOpciones();
   }
+
   selectSection(section: string): void {
     this.selectedSection = section;
   }
