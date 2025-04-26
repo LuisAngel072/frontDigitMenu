d74e39147af7304f7a5655b0710c88d66ab5ff02
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2025-04-26 14:48:28 -0600

    feat(meseros): implement MeserosComponent with order management and integrate ListaPedidosComponent

--- src/app/pages/meseros/meseros.component.ts
+++ src/app/pages/meseros/meseros.component.ts
@@ -1,12 +1,17 @@
+// meseros.component.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListaPedidosComponent } from '../comun-componentes/lista-pedidos/lista-pedidos.component';
+// import { PedidosService } from '../services/pedidos.service';
 
 @Component({
   selector: 'app-meseros',
-  standalone: true,
-  imports: [],
   templateUrl: './meseros.component.html',
-  styleUrl: './meseros.component.css'
+  styleUrls: ['./meseros.component.scss'],
+  standalone: true,
+  imports: [CommonModule, ListaPedidosComponent],
+  // providers: [PedidosService] // Add your services here
 })
 export class MeserosComponent {
-
+  // Your meseros component properties and methods
 }
